259eb9b6634ba4f7b67769acf4295c6aee61406f
Pule Morifi <pumelo.kp@gmail.com>
2024-04-27 01:30:24 +0200

    mimic web

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let about = "About";
+let careers = "Careers";
+let evnts = "Events";
+let prodcts = "Products";
+let supprt = "Support";
+
+document.title = "Frontend Mentor | Loopstudios landing page";
+
+document.getElementById("about").innerHTML = about;
+document.getElementById("careers").innerHTML = careers;
+document.getElementById("events").innerHTML = evnts;
+document.getElementById("products").innerHTML = prodcts;
+document.getElementById("support").innerHTML = supprt;
+
+document.getElementById("footer-about").innerHTML = about;
+document.getElementById("footer-careers").innerHTML = careers;
+document.getElementById("footer-events").innerHTML = evnts;
+document.getElementById("footer-products").innerHTML = prodcts;
+document.getElementById("footer-support").innerHTML = supprt;
+
+document.getElementById("title").innerHTML =
+  "Immersive experiences that deliver";
+
+document.getElementById("the-leader").innerHTML =
+  "The leader in interactive VR";
+
+const information =
+  "Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.";
+
+document.getElementById("com-info").innerHTML = information;
+
+document.getElementById("creation").innerHTML = "Our creations";
+document.getElementById("see-all").innerHTML = "see all";
+
+document.getElementById("footer-info").innerHTML =
+  "© 2021 Loopstudios. All rights reserved.";
